72977260634cb033ddae769640ea135c53948e67
WhyATent <WWyatnt@gmail.com>
2026-03-19 20:23:20 -0700

    a45 Done. The input values for the data#.txt in the data directory seem correct, and produce passing pytests. I will not be changing those this time.

--- main.cpp
+++ main.cpp
@@ -12,6 +12,31 @@ int main()
     cin >> num1 >> num2 >> num3;
 
     // TODO: Find min, max, and med among num1, num2, num3
-
+    if (num1>num2){
+        if (num2>num3){
+            max=num1, med=num2, min=num3;
+        }
+        else{
+            if(num1>num3){
+                max=num1, med=num3, min=num2;
+            }
+            else{
+                max=num3, med=num1, min=num2;
+            }
+        }
+    }
+    else{
+        if (num1>num3){
+            max=num2, med=num1, min=num3;
+        }
+        else{
+            if (num2>num3){
+                max=num2, med=num3, min=num1;
+            }
+            else{
+                max=num3, med=num2, min=num1;
+            }
+        }
+    }
     cout << min << " " << med << " " << max << endl;
 }
